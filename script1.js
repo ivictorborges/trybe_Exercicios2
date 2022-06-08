988df9f96a63ad1ed2fe1cd94cfbce0d59e46443
Victor <v_tord@hotmail.com>
2022-06-08 19:38:43 -0300

    Atualização de Exercício I

--- script1.js
+++ script1.js
@@ -1,14 +1,21 @@
+// PRIMEIRO
 const testingScope = (escopo) => {
-    if (escopo === true) {
-      let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-      ifScope = `${ifScope}
+  if (escopo === true) {
+    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
+    ifScope = `${ifScope}
       ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-      console.log(elseScope);
-    }
-    // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+    console.log(ifScope);
+  } else {
+    let elseScope = `Não devo ser utilizada fora meu escopo (else)`;
+    console.log(elseScope);
   }
+  // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+} testingScope(true);
 
-  testingScope(true);
+// SEGUNDO
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const orderedArray = (a, b) => {
+  return a - b;
+};
+console.log(`Os números ${oddsAndEvens.sort(orderedArray)} se encontram ordenados de forma crescente!`);
