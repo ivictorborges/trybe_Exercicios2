89a26ce1fa35fc99a6d414212267f1b611dc8cfc
Victor <v_tord@hotmail.com>
2022-06-13 18:44:04 -0300

    Inclusão de arquivo JS sobre exercícios

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//REQUISITO 7 ( Qual o menor valor )
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let lowerNumber = numbers [0];
@@ -10,6 +11,16 @@ for ( let index = 1; index < numbers.length; index += 1) {
 
 console.log(lowerNumber);
 
+//BÔNUS ( Bubble Sort )
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-
-
+for (let index = 1; index < numbers.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (numbers[index] > numbers[secondIndex]) {
+        let position = numbers[index];
+        numbers[index] = numbers[secondIndex];
+        numbers[secondIndex] = position;
+      }
+    }
+  }
+  console.log(numbers);
