55faa45ed6381df8c712d98d195676d20c023b54
Victor <v_tord@hotmail.com>
2022-06-13 18:03:35 -0300

    Inclusão de arquivo JS sobre exercícios

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 var a = 2;
 var b = 5;
 
-let sum = a+b;
-let sub = a-b;
-let mult = a*b;
-let div = a/b;
-let mod = a%b;
+let sum = a + b;
+let sub = a - b;
+let mult = a * b;
+let div = a / b;
+let mod = a % b;
 
 // REQUISITO 2
 var numberOne = 18;
@@ -27,10 +27,10 @@ var three = 40;
 if (one > two && one > three) {
     console.log(one);
 }
-else if (two > one && two > three ) {
+else if (two > one && two > three) {
     console.log(two);
 }
-else {console.log(three);}
+else { console.log(three); }
 
 // REQUISITO 4
 var number = 0;
@@ -41,7 +41,7 @@ if (number > 0) {
 else if (number < 0) {
     console.log('Negative');
 }
-else {console.log('zero');}
+else { console.log('zero'); }
 
 // REQUISITO 5
 var angleA = -70;
@@ -56,7 +56,91 @@ if (trianglePositive) {
     if (triangle === 180) {
         console.log('true');
     }
-    else {console.log('false');}
+    else { console.log('false'); }
 }
-else {console.log('Erro');}
+else { console.log('Erro'); }
 
+// REQUISITO 6
+let chessPiece = 'cavalo';
+
+switch (chessPiece.toLowerCase()) {
+    case 'rainha':
+        console.log('Qualquer casa em todas as direções');
+        break;
+    case 'rei':
+        console.log('Uma casa em todas as direções');
+        break;
+    case 'bispo':
+        console.log('Diagonais');
+        break;
+    case 'cavalo':
+        console.log('Anda em "L"');
+        break;
+    case 'torre':
+        console.log('Linha reta em qualquer casa');
+        break;
+    case 'peão':
+        console.log('Apenas uma casa para frente, no primeiro movimento, duas casas');
+        break;
+    default:
+        console.log('Erro, peça inválida');
+        break;
+}
+
+// REQUISITO 7
+let note = 40;
+
+if (note < 0 || note > 100) {
+    console.log('Erro');
+}
+else if (note >= 90) {
+    console.log('A');
+}
+else if (note >= 80) {
+    console.log('B');
+}
+else if (note >= 70) {
+    console.log('C');
+}
+else if (note >= 60) {
+    console.log('D');
+}
+else if (note >= 50) {
+    console.log('E');
+}
+else {
+    console.log('F');
+}
+
+//REQUISITO 8
+const one = 12;
+const two = 11;
+const three = 15;
+let isEven = false;
+
+if (one % 2 === 0 || two % 2 === 0 || three % 2 === 0 ) {
+    isEven = true;
+}
+console.log(isEven);
+
+//REQUISITO 9
+const one = 16;
+const two = 12;
+const three = 14;
+let isOdd = false;
+
+if (one % 2 !== 0 || two % 2 !== 0 || three % 2 !== 0) {
+    isOdd = true;
+}
+console.log(isOdd);
+
+//REQUISITO 10
+const custo = 1;
+const venda = 2;
+
+if (custo < 0 || venda < 0) {
+    console.log('Erro');
+}
+else {
+    console.log(((venda) - (custo*1.2))*1000);
+}
