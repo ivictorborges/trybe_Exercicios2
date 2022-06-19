8d57eb9fd8734294c9092078c23ef95219868496
Victor <v_tord@hotmail.com>
2022-06-19 18:22:28 -0300

    Inclusão de arquivos sobre exercícios

--- script1.js
+++ script1.js
@@ -1,3 +1,4 @@
+// PARTE I
 // PRIMEIRO
 const testingScope = (escopo) => {
   if (escopo === true) {
@@ -19,3 +20,44 @@ const orderedArray = (a, b) => {
   return a - b;
 };
 console.log(`Os números ${oddsAndEvens.sort(orderedArray)} se encontram ordenados de forma crescente!`);
+
+// PARTE II
+// PRIMEIRO
+const factorial = number => number > 1 ? number * factorial(number - 1) : 1; // TERNARY OPERATOR
+console.log(factorial(6));
+
+// SEGUNDO
+const longestWord = (text) => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+console.log(longestWord('Antonio foi ao banheiro e não sabemos o que aconteceu'));
+
+// QUARTO
+const substituaX = (nome) => {
+  const frase = 'Tryber x aqui!';
+  const fraseArray = frase.split(' ');
+  for (let index = 0; index < fraseArray.length; index += 1) {
+    if (fraseArray[index] === 'x') {
+      fraseArray[index] = nome;
+    }
+  }
+  return fraseArray.join(' ');
+};
+
+console.log(substituaX('Victor'));
+
+// QUINTO
+const minhasSkills = (func) => {
+  const skills = ['JavaScript', 'HTML', 'CSS'];
+  let resultado = `
+${func}
+
+  Minhas três principais habilidades são:`;
+
+  skills.forEach((skill) => { // estrutura de repetição para iterar sobre o array de skills
+    resultado = `${resultado}
+    - ${skill}`;
+  }); // reatribui e adiciona a skill atual a variável resultado
+  return resultado;
+};
+
+console.log(minhasSkills(substituaX('Victor')));
