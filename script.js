e08cbd3a05a9d5f21aa0c78c49c7661ff92d627e
Victor <v_tord@hotmail.com>
2022-06-23 23:29:09 -0300

    Atualização de arquivo JS sobre exercícios do Bloco 8, Dia 1

--- script.js
+++ script.js
@@ -46,3 +46,94 @@ const countPoints = (rightAnswerToCount, studentAnswerToCount, callback) => {
     return `Resultado final : ${counter} pontos !`;
 };
 console.log(countPoints(rightAnswers, studentAnswers, checkAnswers));
+
+// BÔNUS
+// PARTE I
+const mage = {
+    healthPoints: 130,
+    intelligence: 45,
+    mana: 125,
+    damage: undefined,
+  };
+  
+const warrior = {
+    healthPoints: 200,
+    strength: 30,
+    weaponDmg: 2,
+    damage: undefined,
+  };
+  
+const dragon = {
+    healthPoints: 350,
+    strength: 50,
+    damage: undefined,
+  };
+  
+const battleMembers = { mage, warrior, dragon };
+
+// REQUISITO 1
+const dragonAttack = (dragon) => {
+    const minDmg = 15;
+    const dragonDmg = Math.floor((Math.random() * (dragon.strength - minDmg + 1) + minDmg));
+    return dragonDmg;
+  };
+
+// REQUISITO 2
+const warriorAttack = (warrior) => {
+    const minDmg = warrior.strength;
+    const maxDmg = minDmg * warrior.weaponDmg;
+    const warriorDamage = Math.floor((Math.random() * (maxDmg - minDmg + 1)) + minDmg);
+    return warriorDamage;
+  };
+
+// REQUISITO 3
+const mageAttack = (mage) => {
+    const mageMana = mage.mana;
+    const minDmg = mage.intelligence;
+    const maxDmg = minDmg * 2;
+    const turnStats = {
+      manaSpent: 0,
+      damageDealt: 'Not enough mana...',
+    };
+  
+    if (mageMana > 15) {
+      const mageDamage = Math.floor((Math.random() * (maxDmg - minDmg + 1)) + minDmg);
+      turnStats.manaSpent = 15;
+      turnStats.damageDealt = mageDamage;
+      return turnStats;
+    }
+    return turnStats;
+  };
+
+// PARTE II
+const gameActions = {
+    // REQUISITO 1
+    warriorTurn: (warriorAttack) => {
+      const warriorDamage = warriorAttack(warrior);
+      warrior.damage = warriorDamage;
+      dragon.healthPoints -= warriorDamage;
+    },
+    // REQUISITO 2
+    mageTurn: (mageAttack) => {
+      const mageTurnStats = mageAttack(mage);
+      const mageDamage = mageTurnStats.damageDealt;
+      mage.damage = mageDamage;
+      mage.mana -= mageTurnStats.manaSpent;
+      dragon.healthPoints -= mageDamage;
+    },
+    // REQUISITO 3
+    dragonTurn: (dragonAttack) => {
+      const dragonDamage = dragonAttack(dragon);
+      mage.healthPoints -= dragonDamage;
+      warrior.healthPoints -= dragonDamage;
+      dragon.damage = dragonDamage;
+    },
+    // REQUISITO 4
+    turnResults: () => battleMembers,
+  };
+  
+  gameActions.warriorTurn(warriorAttack);
+  gameActions.mageTurn(mageAttack);
+  gameActions.dragonTurn(dragonAttack);
+  console.log(gameActions.turnResults());
+
